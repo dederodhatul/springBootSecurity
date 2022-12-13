6fef0b4a175911a7f50b169d32dda27e532a80c4
dederodhatul <dederodhatulfarida@gmail.com>
2022-12-13 11:11:05 +0700

    add projek spring initializr

---
+++ src/main/java/com/indocyber/LatihanTrollMarketApplication.java
@@ -0,0 +1,13 @@
+package com.indocyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanTrollMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanTrollMarketApplication.class, args);
+	}
+
+}
